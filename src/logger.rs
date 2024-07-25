a2c864b5313bdad1f1341d7321abe96a78118ec1
lambor590 <lambor590@gmail.com>
2024-07-26 01:50:50 +0200

    formatting

--- src/logger.rs
+++ src/logger.rs
@@ -3,16 +3,27 @@ use yansi::Paint;
 pub struct Logger;
 
 impl Logger {
-
     pub fn info(message: &str) {
-        println!("{} {}", " INFO ".bold().on_black().bright_blue().invert(), message);
+        println!(
+            "{} {}",
+            " INFO ".bold().on_black().bright_blue().invert(),
+            message
+        );
     }
 
-    // pub fn warn(message: &str) {
-    //     println!("{} {}", " WARN ".bold().on_black().bright_yellow().invert(), message);
-    // }
+    pub fn warn(message: &str) {
+        println!(
+            "{} {}",
+            " WARN ".bold().on_black().bright_yellow().invert(),
+            message
+        );
+    }
 
-    // pub fn error(message: &str) {
-    //     println!("{} {}", " ERROR ".bold().on_black().bright_red().invert(), message);
-    // }
+    pub fn error(message: &str) {
+        println!(
+            "{} {}",
+            " ERROR ".bold().on_black().bright_red().invert(),
+            message
+        );
+    }
 }
