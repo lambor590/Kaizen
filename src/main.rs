d2ca75f86ca635c8408f98eb9fbb675deeff9b5b
lambor590 <lambor590@gmail.com>
2024-07-26 01:56:38 +0200

    change console title & catch update errors

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 mod logger;
+mod updater;
 
+use crossterm::{execute, terminal::SetTitle};
 use logger::Logger;
-
-fn check_update() -> Result<(), Box<dyn ::std::error::Error>> {
-    // TODO: Implementar
-    Ok(())
-}
+use updater::Updater;
 
 fn main() {
+    execute!(
+        std::io::stdout(),
+        SetTitle(format!("Ghost Toolkit - v{}", env!("CARGO_PKG_VERSION")))
+    )
+    .unwrap();
     Logger::info("Primera versión pública de Ghost Toolkit hecho en Rust");
 
-    check_update().unwrap();
+    if let Err(e) = Updater::check_updates() {
+        Logger::error(format!("Error al comprobar actualizaciones: {}", &e).as_str());
+    }
 
     std::io::stdin().read_line(&mut String::new()).unwrap();
 }
