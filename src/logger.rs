d39a48cd4808ca01657263917ae856cc5a59301a
lambor590 <lambor590@gmail.com>
2024-07-26 21:11:49 +0200

    format

--- src/logger.rs
+++ src/logger.rs
@@ -4,26 +4,14 @@ pub struct Logger;
 
 impl Logger {
     pub fn info(message: &str) {
-        println!(
-            "{} {}",
-            " INFO ".bold().black().on_blue(),
-            message
-        );
+        println!("{} {}", " INFO ".bold().black().on_blue(), message);
     }
 
     pub fn warn(message: &str) {
-        println!(
-            "{} {}",
-            " WARN ".bold().black().on_yellow(),
-            message
-        );
+        println!("{} {}", " WARN ".bold().black().on_yellow(), message);
     }
 
     pub fn error(message: &str) {
-        println!(
-            "{} {}",
-            " ERROR ".bold().black().on_red(),
-            message
-        );
+        println!("{} {}", " ERROR ".bold().black().on_red(), message);
     }
 }
