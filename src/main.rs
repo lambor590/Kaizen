ec3febe274b9759df3b01e4b9fe6a8dfe6a96fbc
lambor590 <lambor590@gmail.com>
2024-07-27 19:14:25 +0200

    simplify main file

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,29 @@
+mod commands;
+mod console;
 mod logger;
 mod updater;
+mod windows_api;
 
-use crossterm::{
-    cursor, execute,
-    style::{Color, Stylize},
-    terminal::{Clear, ClearType, SetTitle},
-};
+use console::Console;
+use crossterm::{cursor, execute};
 use logger::Logger;
-use std::{
-    io::{stdin, stdout},
-    thread,
-    time::Duration,
-};
+use std::io::stdout;
 use updater::Updater;
+use windows_api::WindowsAPI;
 
 fn main() {
-    execute!(
-        stdout(),
-        SetTitle(format!("Ghost Toolkit - v{}", env!("CARGO_PKG_VERSION")))
-    )
-    .unwrap();
+    Console::set_title(&format!("Ghost Toolkit - v{}", &env!("CARGO_PKG_VERSION")));
+    execute!(stdout(), &cursor::MoveToRow(7), &cursor::SavePosition).unwrap();
+    Console::animate_logo();
 
-    let title: [&str; 6] = [
-        "  _____ _______ ",
-        " / ____|__   __|",
-        "| |  __   | |   ",
-        "| | |_ |  | |   ",
-        "| |__| |  | |   ",
-        " \\_____|  |_|   ",
-    ];
-
-    let term_width: usize = crossterm::terminal::size().map_or(80, |(w, _)| w as usize);
-
-    let rainbow_colors: [Color; 6] = [
-        Color::Red,
-        Color::Magenta,
-        Color::Yellow,
-        Color::Green,
-        Color::Cyan,
-        Color::Blue,
-    ];
-
-    execute!(stdout(), cursor::Hide).unwrap();
-
-    for color_step in (0..rainbow_colors.len() * 4).rev() {
-        for &line in &title {
-            let padding: usize = term_width.saturating_sub(line.len()) / 2;
-            print!("{:padding$}", "", padding = padding);
-            for (ch_idx, ch) in line.chars().enumerate() {
-                print!(
-                    "{}",
-                    ch.with(rainbow_colors[(color_step + ch_idx / 2) % rainbow_colors.len()])
-                );
-            }
-            println!();
-        }
-        thread::sleep(Duration::from_millis(60));
-        execute!(stdout(), cursor::MoveToPreviousLine(title.len() as u16)).unwrap();
+    if let Err(e) = &Updater::check_updates() {
+        Logger::error(&format!("Error al comprobar actualizaciones: {}", &e).as_str());
     }
 
-    execute!(stdout(), cursor::MoveToRow(7)).unwrap();
+    Console::sleep_secs(2);
+    Console::clear();
 
-    if let Err(e) = Updater::check_updates() {
-        Logger::error(format!("Error al comprobar actualizaciones: {}", &e).as_str());
-    }
+    WindowsAPI::check_admin();
 
-    thread::sleep(Duration::from_secs(2));
-    execute!(stdout(), cursor::MoveToPreviousLine(1)).unwrap();
-    execute!(stdout(), Clear(ClearType::CurrentLine)).unwrap();
-
-    stdin().read_line(&mut String::new()).unwrap();
+    Console::menu();
 }
