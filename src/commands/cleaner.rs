fd831e81ad39414f73e8b263a98f416fbf9efb05
lambor590 <lambor590@gmail.com>
2024-07-27 20:33:51 +0200

    show total freed space

--- src/commands/cleaner.rs
+++ src/commands/cleaner.rs
@@ -9,6 +9,7 @@ impl Cleaner {
         let temp_dir: std::path::PathBuf = std::env::temp_dir();
         let mut total_files: u64 = 0;
         let mut deleted_files: i32 = 0;
+        let mut freed_space: u64 = 0;
         let mut progress_bar: Option<indicatif::ProgressBar> = None;
 
         if let Ok(entries) = std::fs::read_dir(&temp_dir) {
@@ -22,9 +23,13 @@ impl Cleaner {
             for entry in entries.flatten() {
                 let path: &std::path::PathBuf = &entry.path();
                 let result: Result<(), std::io::Error> = if path.is_file() {
+                    if let Ok(metadata) = std::fs::metadata(path) {
+                        freed_space += metadata.len();
+                    }
                     std::fs::remove_file(path)
                 } else if path.is_dir() {
-                    remove_dir_all(path)
+                    let dir_size = Self::get_dir_size(path);
+                    remove_dir_all(path).map(|_| freed_space += dir_size)
                 } else {
                     Ok(())
                 };
@@ -41,12 +46,31 @@ impl Cleaner {
 
         Console::clear();
 
+        let freed_space_mb = freed_space as f64 / 1_048_576.0;
         Logger::warn("Algunos archivos no se pueden eliminar al estar en uso por otros programas.");
         Logger::info(&format!(
             "Archivos eliminados: {}/{}",
             deleted_files, total_files
         ));
+        Logger::info(&format!("Espacio liberado: {:.2} MB", freed_space_mb));
 
         Ok(())
+    }
+
+    fn get_dir_size(path: &std::path::PathBuf) -> u64 {
+        let mut size = 0;
+        if let Ok(entries) = std::fs::read_dir(path) {
+            for entry in entries.flatten() {
+                let path = entry.path();
+                if path.is_file() {
+                    if let Ok(metadata) = std::fs::metadata(&path) {
+                        size += metadata.len();
+                    }
+                } else if path.is_dir() {
+                    size += Self::get_dir_size(&path);
+                }
+            }
+        }
+        size
     }
 }
