fa769d7dfb5a4a4848e98d5925fec4ae34e67ecf
lambor590 <lambor590@gmail.com>
2024-08-10 01:42:25 +0200

    add: implement Countdown component with timer and optional finish callback

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'preact/hooks'
+
+interface Props {
+  initialNumber: number;
+  onFinish?: () => void;
+}
+
+export default function Countdown({ initialNumber, onFinish }: Props) {
+  const [timeLeft, setTimeLeft] = useState(initialNumber);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTimeLeft((time) => {
+        if (time !== 0) return time - 1;
+        clearInterval(timer);
+        onFinish && onFinish();
+        return 0;
+      });
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="countdown font-mono text-6xl flex flex-col items-center">
+      <span style={{ "--value": timeLeft }}></span>
+    </div>
+  )
+}
