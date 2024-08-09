c20f68ba35ab9ee24ceb6bc7f086d40ebe2c8a07
lambor590 <lambor590@gmail.com>
2024-08-10 01:39:05 +0200

    add: implement splash screen and main application window

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,25 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{Manager, Window};
+
+#[tauri::command]
+async fn close_splash(window: Window) {
+    window
+        .get_window("splash")
+        .expect("no window labeled 'splash' found")
+        .close()
+        .unwrap();
+    
+    window
+        .get_window("main")
+        .expect("no window labeled 'main' found")
+        .show()
+        .unwrap();
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![close_splash])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
