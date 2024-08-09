40979199ea10828179e9daa1957b225fc1c42519
lambor590 <lambor590@gmail.com>
2024-08-10 01:40:36 +0200

    add: create main App component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import UpdateModal from "./components/UpdateModal";
+
+function App() {
+
+  return (
+    <div>
+      <UpdateModal />
+      <button className="btn">Test</button>
+    </div>
+  );
+}
+
+export default App;
