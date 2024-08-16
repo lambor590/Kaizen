b3e471154def9734458e36abaed4c56510960d95
lambor590 <lambor590@gmail.com>
2024-08-17 01:08:02 +0200

    chore: simplify Update Modal logic

--- src/components/UpdateModal.tsx
+++ src/components/UpdateModal.tsx
@@ -1,20 +1,11 @@
-import { useState, useEffect } from 'preact/hooks';
-import { checkUpdate, installUpdate } from '@tauri-apps/api/updater';
+import { installUpdate } from '@tauri-apps/api/updater';
 import Modal from './Modal'
 import Countdown from './Countdown'
 
 export default function UpdateModal() {
-  const [isOpen, setIsOpen] = useState(false)
-
-  useEffect(() => {
-    (async () => {
-      const self = await checkUpdate();
-      if (self.shouldUpdate) setIsOpen(true)
-    })()
-  })
 
   return (
-    <Modal isOpen={isOpen} title="Actualización disponible" message="La aplicación se va a reiniciar para aplicar la actualización." isDismissable={false}>
+    <Modal isOpen={true} title="Actualización disponible" message="La aplicación se va a reiniciar para aplicar la actualización." isDismissable={false}>
       <Countdown initialNumber={3} onFinish={async () => await installUpdate()} />
     </Modal>
   )
