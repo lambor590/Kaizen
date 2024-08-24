7fd32bbef0d59c5a82405a130817cf7a6fa0baae
lambor590 <lambor590@gmail.com>
2024-08-25 01:22:06 +0200

    fix: remove unnecessary whitespace in UpdateModal

--- src/components/UpdateModal.tsx
+++ src/components/UpdateModal.tsx
@@ -3,7 +3,6 @@ import Modal from '@/components/generic/Modal'
 import Countdown from '@/components/generic/Countdown'
 
 export default function UpdateModal() {
-
   return (
     <Modal isOpen={true} title="Actualización disponible" message="La aplicación se va a reiniciar para aplicar la actualización." isDismissable={false}>
       <Countdown initialNumber={3} onFinish={async () => await installUpdate()} />
