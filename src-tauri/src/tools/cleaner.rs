6f1b8979faae1457a2f06804c4bce43c5fb3e1a2
lambor590 <lambor590@gmail.com>
2024-08-25 17:01:53 +0200

    refactor: streamline file deletion logic and improve memory usage

--- src-tauri/src/tools/cleaner.rs
+++ src-tauri/src/tools/cleaner.rs
@@ -1,5 +1,5 @@
 use serde_json::json;
-use std::fs::remove_dir_all;
+use std::fs::{self, remove_dir_all};
 use std::path::PathBuf;
 use tauri::{Runtime, Window};
 
@@ -11,18 +11,18 @@ pub async fn run_cleaner<R: Runtime>(window: Window<R>, dirs: Vec<String>) -> Ka
     let mut total_files: i32 = 0;
     let mut freed_space: f64 = 0.0;
 
-    for dir in dirs {
+    for dir in &dirs {
         let path: PathBuf = PathBuf::from(dir);
 
-        if let Ok(entries) = std::fs::read_dir(&path) {
-            for entry in entries.flatten() {
+        if let Ok(entries) = fs::read_dir(&path) {
+            entries.filter_map(Result::ok).for_each(|entry| {
                 total_files += 1;
 
                 let path: PathBuf = entry.path();
                 if let Ok(metadata) = path.metadata() {
                     let file_size: f64 = metadata.len() as f64;
                     let result: Result<(), std::io::Error> = if metadata.is_file() {
-                        std::fs::remove_file(&path)
+                        fs::remove_file(&path)
                     } else {
                         remove_dir_all(&path)
                     };
@@ -33,18 +33,24 @@ pub async fn run_cleaner<R: Runtime>(window: Window<R>, dirs: Vec<String>) -> Ka
                     }
 
                     let mb: f64 = freed_space / 1024.0 / 1024.0;
+                    let freed_space_str: String = if mb > 1024.0 {
+                        format!("{:.2} GB", mb / 1024.0)
+                    } else {
+                        format!("{:.2} MB", mb)
+                    };
+
                     window
                         .emit(
                             "cleaner-data",
                             json!({
                                 "deleted_files": deleted_files,
                                 "total_files": total_files,
-                                "freed_space": if mb > 1024.0 { format!("{:.2} GB", mb / 1024.0) } else { format!("{:.2} MB", mb) }
+                                "freed_space": freed_space_str
                             }),
                         )
                         .unwrap();
                 }
-            }
+            });
         }
     }
 
