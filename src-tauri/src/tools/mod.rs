2d2f4d24bc1fd99cb4b88b27aaaea9ab0f6aba05
lambor590 <lambor590@gmail.com>
2024-11-01 12:34:06 +0100

    refactor: clean up unused imports and simplify serialization

--- src-tauri/src/tools/mod.rs
+++ src-tauri/src/tools/mod.rs
@@ -1,9 +1,5 @@
 use serde::ser::SerializeStruct;
 use serde::{Serialize, Serializer};
-use tauri::{
-    plugin::{Builder, TauriPlugin},
-    Runtime,
-};
 use thiserror::Error;
 
 pub mod cleaner;
@@ -25,21 +21,8 @@ impl Serialize for KaizenSerializableError {
     where
         S: Serializer,
     {
-        let mut state: <S as Serializer>::SerializeStruct =
-            serializer.serialize_struct("KaizenSerializableError", 1)?;
+        let mut state = serializer.serialize_struct("KaizenSerializableError", 1)?;
         state.serialize_field("message", &self.to_string())?;
         state.end()
     }
-}
-
-pub fn init<R: Runtime>() -> TauriPlugin<R> {
-    Builder::new("tools")
-        .invoke_handler(tauri::generate_handler![
-            cleaner::run_cleaner,
-            downloader::run_downloader,
-            downloader::get_video_data,
-            downloader::check_downloader_deps,
-            downloader::install_downloader_deps,
-        ])
-        .build()
 }
