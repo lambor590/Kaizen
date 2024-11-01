a6dad2cb1e5b8f438108fb00a0e54333c26d441e
lambor590 <lambor590@gmail.com>
2024-11-01 12:37:15 +0100

    refactor: update Modal component props and improve JSX structure

--- src/components/generic/Modal.tsx
+++ src/components/generic/Modal.tsx
@@ -1,26 +1,25 @@
-import { ComponentChildren } from "preact";
+import type { JSX } from 'solid-js';
 
 interface Props {
-  isOpen: boolean;
+  isOpen?: boolean;
   title?: string;
   message: string;
-  children?: ComponentChildren;
   isDismissable?: boolean;
   onClose?: () => void;
+  children?: JSX.Element;
 }
 
-export default function Modal(props: Props) {
+export default function Modal({ isOpen = false, title = "Mensaje", message, isDismissable = true, onClose, children }: Props) {
   return (
-    <dialog className={`modal overflow-hidden backdrop-blur-sm backdrop-brightness-90 ${props.isOpen ? 'modal-open' : ''}`}>
-      <div className="modal-box text-left">
-        <h3 className="font-bold text-lg">{props.title}</h3>
-        <p className="pt-4">{props.message}</p>
-        {props.children}
-        {
-          props.isDismissable &&
-          <div className="modal-action">
+    <dialog class={`modal overflow-hidden backdrop-blur-sm backdrop-brightness-90 ${isOpen ? "modal-open" : ""}`}>
+      <div class="modal-box text-left">
+        {title && <h3 class="font-bold text-lg">{title}</h3>}
+        <p class="pt-4">{message}</p>
+        {children}
+        {isDismissable &&
+          <div class="modal-action">
             <form method="dialog">
-              <button className="btn" onClick={props.onClose}>Cerrar</button>
+              <button class="btn" onClick={() => onClose?.()}>Cerrar</button>
             </form>
           </div>
         }
