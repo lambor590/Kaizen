7fd9e000610cb8b02c957b43372efb2bf4cfc2b1
lambor590 <lambor590@gmail.com>
2024-11-01 12:40:17 +0100

    refactor: update Checkbox component to use Solid.js types and improve props handling

--- src/components/generic/Checkbox.tsx
+++ src/components/generic/Checkbox.tsx
@@ -1,25 +1,25 @@
-import { ComponentChildren } from "preact";
+import type { JSX } from 'solid-js';
 
 interface Props {
-  text: string;
+  text?: string;
   checked?: boolean;
   disabled?: boolean;
-  onChange?: (e: Event) => void;
-  children?: ComponentChildren;
+  onChange?: () => void;
+  children?: JSX.Element;
 }
 
-export default function CheckBox(props: Props) {
+export default function Checkbox({ text, checked = false, disabled = false, onChange, children }: Props) {
   return (
-    <label className="cursor-pointer label justify-normal">
+    <label class="cursor-pointer label justify-normal">
       <input
         type="checkbox"
-        defaultChecked={props.checked}
-        disabled={props.disabled}
-        onChange={(e) => props.onChange?.(e)}
-        className="checkbox mr-4"
+        checked={checked}
+        disabled={disabled}
+        onChange={onChange}
+        class="checkbox mr-4"
       />
-      <span>{props.text}</span>
-      {props.children}
+      <span>{text}</span>
+      {children}
     </label>
   );
 }
