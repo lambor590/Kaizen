2658330a44eaad388981b3cf5a9c5d7a9e7d4bbd
lambor590 <lambor590@gmail.com>
2024-11-01 12:34:49 +0100

    refactor: update close_splash to use AppHandle and improve window management

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,36 +3,40 @@
 mod tools;
 mod windows_api;
 
-use tauri::{Manager, Window};
+use tauri::{AppHandle, Manager};
 use windows_api::WindowsAPI;
 
 #[tauri::command]
-async fn close_splash(window: Window) {
-    window
-        .get_window("splash")
-        .expect("no window labeled 'splash' found")
-        .close()
-        .unwrap();
-
-    window
-        .get_window("main")
-        .expect("no window labeled 'main' found")
-        .show()
-        .unwrap();
+async fn close_splash(app: AppHandle) -> Result<(), tauri::Error> {
+    if let Some(splash) = app.get_webview_window("splash") {
+        splash.close()?;
+        app.get_webview_window("main").unwrap().show()?;
+    }
+    Ok(())
 }
 
 #[tauri::command]
 async fn check_admin(ask: bool) -> bool {
-    if ask {
-        WindowsAPI::restart_as_admin()
-    }
-    WindowsAPI::check_admin()
+    ask && WindowsAPI::restart_as_admin() || WindowsAPI::check_admin()
 }
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tools::init())
-        .invoke_handler(tauri::generate_handler![close_splash, check_admin])
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_os::init())
+        .invoke_handler(tauri::generate_handler![
+            close_splash,
+            check_admin,
+            tools::cleaner::run_cleaner,
+            tools::downloader::run_downloader,
+            tools::downloader::get_video_data,
+            tools::downloader::check_downloader_deps,
+            tools::downloader::install_downloader_deps
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
